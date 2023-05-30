213f130b488fee2dcb17291c58932fdac2724aab
Mathuu17 <2105731@ucc.edu.ar>
2023-05-30 11:16:26 -0300

    cambio en js

--- Juego/juego.js
+++ Juego/juego.js
@@ -2,80 +2,78 @@ const canvas = document.querySelector('canvas')
 canvas.height = 700;
 canvas.width = 680;
 const c2d = canvas.getContext('2d');
-const C_puntos = document.querySelector('#C_puntos')
+const C_puntos = document.querySelector('#C_puntos');
+const img1 = document.getElementById("pac");
+const img2 = document.getElementById("fant");
+const img3 = document.getElementById("ladr");
 
 class Pacman {
     constructor({posicion, velocidad}) {
-        this.cuerpo = 12
-        this.posicion = posicion
-        this.velocidad = velocidad
+        this.cuerpo = 12;
+        this.posicion = posicion;
+        this.velocidad = velocidad;
     }
+
     Dibujar() {
-        c2d.beginPath()
-        c2d.arc(this.posicion.x, this.posicion.y, this.cuerpo, 0, Math.PI*2 )
-        c2d.fillStyle = '#ffe600'
-        c2d.fill()
-        c2d.closePath()
+        c2d.drawImage(img1, this.posicion.x-15, this.posicion.y-15,30,30);
     }
+
     actualizar() {
         this.Dibujar()
-        this.posicion.x += this.velocidad.x
-        this.posicion.y += this.velocidad.y
+        this.posicion.x += this.velocidad.x;
+        this.posicion.y += this.velocidad.y;
     }
 }
+
 class Fantasma {
     constructor({posicion, velocidad}) {
-        this.cuerpo = 12
-        this.posicion = posicion
-        this.velocidad = velocidad
-        this.Colisiones_anteriores = []
+        this.cuerpo = 12;
+        this.posicion = posicion;
+        this.velocidad = velocidad;
+        this.Colisiones_anteriores = [];
     }
+
     Dibujar2() {
-        c2d.beginPath()
-        c2d.arc(this.posicion.x, this.posicion.y, this.cuerpo, 0, Math.PI * 2)
-        c2d.arc(this.posicion.x+6, this.posicion.y+11, 6, 0, Math.PI * 2)
-        c2d.arc(this.posicion.x-6, this.posicion.y+11, 6, 0, Math.PI * 2)
-        c2d.fillStyle = '#00dcdc'
-        c2d.fillRect(this.posicion.x-12, this.posicion.y,24,10)
-        c2d.fill()
-        c2d.closePath()
+        c2d.drawImage(img2, this.posicion.x-15, this.posicion.y-15,30,30);
     }
+
     actualizar() {
         this.Dibujar2()
-        this.posicion.x += this.velocidad.x
-        this.posicion.y += this.velocidad.y
+        this.posicion.x += this.velocidad.x;
+        this.posicion.y += this.velocidad.y;
     }
 }
+
 class Monedas {
     constructor({posicion}) {
-        this.cuerpo = 5
-        this.posicion = posicion
+        this.cuerpo = 5;
+        this.posicion = posicion;
     }
+
     Dibujar3() {
-        c2d.beginPath()
-        c2d.arc(this.posicion.x, this.posicion.y, this.cuerpo, 0, Math.PI * 2)
-        c2d.fillStyle = 'rgb(255,183,0)'
-        c2d.fill()
-        c2d.closePath()
+        c2d.beginPath();
+        c2d.arc(this.posicion.x, this.posicion.y, this.cuerpo, 0, Math.PI * 2);
+        c2d.fillStyle = 'rgb(255,183,0)';
+        c2d.fill();
+        c2d.closePath();
     }
 }
 
 class Paredes {
-    static width = 40
-    static height = 40
+    static width = 40;
+    static height = 40;
+
     constructor({posicion}) {
-        this.posicion = posicion
-        this.width = 40
-        this.height = 40
+        this.posicion = posicion;
+        this.width = 40;
+        this.height = 40;
     }
+
     Dibujar4() {
-        c2d.lineWidth=4
-        c2d.fillStyle = '#190083'
-        c2d.fillRect(this.posicion.x, this.posicion.y, this.width, this.height)
-        c2d.strokeStyle = '#9700ff'
-        c2d.strokeRect(this.posicion.x, this.posicion.y, this.width, this.height)
+        c2d.drawImage(img3, this.posicion.x, this.posicion.y,40,40);
     }
 }
+
 /**
  * Hace que el pacman tengan una posicion y velocidad predeterminda
  * @method pacman
@@ -124,7 +122,7 @@ const fantasma =
             },
             velocidad: {x: 0, y: -2}
         })
-    ]
+    ];
 
 //Esqueleto del laberinto (#) son las paredes,(º) son las monedas y el espacio vacio es donde empieza el pacman
 const laberinto = [
@@ -146,9 +144,9 @@ const laberinto = [
     ['#', 'º', 'º', 'º', 'º', 'º', 'º', 'º', 'º', 'º', 'º', 'º', 'º', 'º', 'º', 'º', '#'],
     ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#']
 ]
-let ul_control = ''
-const moneda = []
-const paredes = []
+
+const moneda = [];
+const paredes = [];
 /**
  * Creacion del laberinto
  * @method laberinto
@@ -175,9 +173,10 @@ laberinto.forEach((row, i) => {
                 break
         }
     })
-})
+});
 
 //Controles
+let ul_control = '';
 /**
  * Hace que los controles W,A,S,D sean falsas por predeterminado
  * @method controles
@@ -237,15 +236,16 @@ addEventListener('keyup', ({key}) => {
  * @return Devuelve contra que pared choco el circulo
  */
 function circulo_colisiona({circulo, cuadrado}) {
-    const padding = Paredes.width / 2 - circulo.cuerpo - 1
-
+    const padding = Paredes.width / 2 - circulo.cuerpo - 1;
     return (circulo.posicion.y - circulo.cuerpo + circulo.velocidad.y <= cuadrado.posicion.y + cuadrado.height + padding &&
         circulo.posicion.x + circulo.cuerpo + circulo.velocidad.x >= cuadrado.posicion.x - padding &&
         circulo.posicion.y + circulo.cuerpo + circulo.velocidad.y >= cuadrado.posicion.y - padding &&
-        circulo.posicion.x - circulo.cuerpo + circulo.velocidad.x <= cuadrado.posicion.x + cuadrado.width + padding)
+        circulo.posicion.x - circulo.cuerpo + circulo.velocidad.x <= cuadrado.posicion.x + cuadrado.width + padding);
 }
-let puntos = 0
-let animacion2
+
+let puntos = 0;
+let animacion2;
+
 /**
  * Desarrollo del juego
  * @method animacion
@@ -255,7 +255,7 @@ function animacion() {
     animacion2 = requestAnimationFrame(animacion)
     // Limpia los movimientos anteriores del pacman
     c2d.clearRect(0, 0, canvas.width, canvas.height)
-
+    pacman.actualizar()
     //Detecta la colision con la pared en direccion -Y
     if (controles.w.presionado && ul_control === 'w') {
         for (let i = 0; i < paredes.length; i++) {
@@ -264,12 +264,15 @@ function animacion() {
                 circulo: {...pacman, velocidad: {x: 0, y: -2}},
                 cuadrado: Paredes
             })
-            ) {pacman.velocidad.y = 0
+            ) {
+                pacman.velocidad.y = 0;
                 break
             } else {
-                pacman.velocidad.y = -2
+                pacman.velocidad.y = -2;
             }
         }
+
+
     }
 
     //Detecta la colision con la pared en direccion Y
@@ -280,10 +283,11 @@ function animacion() {
                 circulo: {...pacman, velocidad: {x: 0, y: 2}},
                 cuadrado: Paredes
             })
-            ) {pacman.velocidad.y = 0
+            ) {
+                pacman.velocidad.y = 0;
                 break
             } else {
-                pacman.velocidad.y = 2
+                pacman.velocidad.y = 2;
             }
         }
     }
@@ -296,12 +300,14 @@ function animacion() {
                 circulo: {...pacman, velocidad: {x: -2, y: 0}},
                 cuadrado: Paredes
             })
-            ) {pacman.velocidad.x = 0
+            ) {
+                pacman.velocidad.x = 0;
                 break
             } else {
-                pacman.velocidad.x = -2
+                pacman.velocidad.x = -2;
             }
         }
+
     }
 
     //Detecta la colision con la pared en direccion X
@@ -312,10 +318,11 @@ function animacion() {
                 circulo: {...pacman, velocidad: {x: 2, y: 0}},
                 cuadrado: Paredes
             })
-            ) {pacman.velocidad.x = 0
+            ) {
+                pacman.velocidad.x = 0;
                 break
             } else {
-                pacman.velocidad.x = 2
+                pacman.velocidad.x = 2;
             }
         }
     }
@@ -327,10 +334,10 @@ function animacion() {
     moneda.forEach((monedas, i) => {
         monedas.Dibujar3()
         if (Math.hypot(monedas.posicion.x - pacman.posicion.x,
-                monedas.posicion.y - pacman.posicion.y) < monedas.cuerpo + pacman.cuerpo) {
+            monedas.posicion.y - pacman.posicion.y) < monedas.cuerpo + pacman.cuerpo) {
             moneda.splice(i, 1)
-            puntos += 5
-            C_puntos.innerHTML = puntos
+            puntos += 5;
+            C_puntos.innerHTML = puntos;
         }
     })
 
@@ -345,11 +352,11 @@ function animacion() {
             cuadrado: Paredes
         })
         ) {
-            pacman.velocidad.x = 0
-            pacman.velocidad.y = 0
+            pacman.velocidad.x = 0;
+            pacman.velocidad.y = 0;
         }
     })
-    pacman.actualizar()
+
     /**
      * Detecta la colision entre fantasma y pacman
      * @method fantasma
@@ -363,14 +370,14 @@ function animacion() {
                 fantasma.posicion.x - pacman.posicion.x,
                 fantasma.posicion.y - pacman.posicion.y) < fantasma.cuerpo + pacman.cuerpo) {
             cancelAnimationFrame(animacion2)
-            alert("!!! Game Over ¡¡¡")
-            alert("Un fantasma te a pillado")
+            alert("!!! Game Over ¡¡¡");
+            alert("Un fantasma te a pillado");
         }
         //Condicion para ganar
         if (moneda.length === 0) {
             cancelAnimationFrame(animacion2)
-            alert("!!! You Win ¡¡¡")
-            alert("Has comido todos los pac-dots")
+            alert("!!! You Win ¡¡¡");
+            alert("Has comido todos los pac-dots");
         }
 
         const colision = []
@@ -435,26 +442,28 @@ function animacion() {
             const direccion = caminos[Math.floor(Math.random() * caminos.length)]
             switch (direccion) {
                 case'arriba':
-                    fantasma.velocidad.x = 0
-                    fantasma.velocidad.y = -2
+                    fantasma.velocidad.x = 0;
+                    fantasma.velocidad.y = -2;
                     break
                 case'abajo':
-                    fantasma.velocidad.x = 0
-                    fantasma.velocidad.y = 2
+                    fantasma.velocidad.x = 0;
+                    fantasma.velocidad.y = 2;
                     break
                 case'izq':
-                    fantasma.velocidad.x = -2
-                    fantasma.velocidad.y = 0
+                    fantasma.velocidad.x = -2;
+                    fantasma.velocidad.y = 0;
                     break
                 case'der':
-                    fantasma.velocidad.x = 2
-                    fantasma.velocidad.y = 0
+                    fantasma.velocidad.x = 2;
+                    fantasma.velocidad.y = 0;
                     break
 
             }
             fantasma.Colisiones_anteriores = []
         }
     })
+
 }
+
 animacion()
 
